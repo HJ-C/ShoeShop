82b1a7b308b92675f1c82e355253c47f1f22a868
HJ-C <swch56@naver.com>
2022-06-09 23:13:41 +0900

    redux1

--- src/component/Cart.js
+++ src/component/Cart.js
@@ -1,14 +1,19 @@
 import {Table} from 'react-bootstrap'
-import { useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
+import { changeName, changeAge, changeCount } from '../store'
 
 function Cart(){
 
 
-    let state = useSelector((state)=>{
-        return state
-    })
+    let state = useSelector((state)=>{ return state })
+    //useDispatch : store.js로 요청 보내주는 함수
+    let dispatch = useDispatch()
+
 
     return (
+    <>
+    {state.user.name} {state.user.age}의 장바구니
+    <button onClick={()=>{dispatch(changeAge(1))}}>버튼</button>
     <Table>
         <thead>
             <tr>
@@ -25,11 +30,17 @@ function Cart(){
                         <td>1</td>
                         <td>{state.cart[i].name}</td>
                         <td>{state.cart[i].count}</td>
+                        <td>
+                            <button onClick={()=>{
+                            dispatch(changeCount(state.cart[i].id))
+                            }}>+</button>
+                        </td>
                     </tr>
                 )
             }
         </tbody>
     </Table> 
+    </>
     )
 }
 
--- src/store.js
+++ src/store.js
@@ -1,16 +1,42 @@
 import { configureStore, createSlice } from '@reduxjs/toolkit'
 
 
+let user = createSlice({
+    name : 'user',
+    initialState : {name : 'kim', age:20},
+    reducers :  {
+        changeName(state){
+            state.name = 'park'
+        },
+        changeAge(state,action){
+            state.age+=action.payload;
+        }
+    }
+})
+// cart.js에 changeAge(1) 기입  
+export let {changeName, changeAge} = user.actions
+
 let cart = createSlice({
   name : 'cart',
   initialState : [
     {id : 0, name : 'White and Black', count : 2},
     {id : 2, name : 'Grey Yordan', count : 1}
-  ]
+  ],
+  reducers: {
+      changeCount(state,action){
+          let num = state.findIndex((a)=>{
+              return a.id === action.payload
+          })
+          state[num].count++
+      }
+  }
 })
+
+export let {changeCount} = cart.actions
 
 export default configureStore({
   reducer: {
+    user: user.reducer,
     cart : cart.reducer
   }
 }) 
